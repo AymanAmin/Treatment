6138608f74dd3ddca6aa1342fd51b6d5f2be91d5
Ayman Amin <Ayman1793@hotmail.com>
2020-04-12 17:52:26 +0300

    Ayman (Notification Change)

---
+++ Treatment/Pages/Setting/UserManagment/MyNotifications.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterEn.Master" AutoEventWireup="true" CodeBehind="MyNotifications.aspx.cs" Inherits="Treatment.Pages.Setting.UserManagment.MyNotifications" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+     <title><% = Treatment.Classes.FieldNames.getFieldName("NotificationPage-Title", "ECMS - My Notification") %></title>
+    <script>setTimeout(function () { document.getElementById("header-div").style.display = "none"; }, 1);</script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Body_Holder" runat="server">
+    <div class="col-xl-12 col-md-12">
+        <div class="card latest-update-card">
+            <div class="card-header">
+                <h5>All Notifications</h5>
+                <div class="card-header-right">
+                    <ul class="list-unstyled card-option">
+                        <li><i class="fa fa fa-wrench open-card-option"></i></li>
+                        <li><i class="fa fa-window-maximize full-card"></i></li>
+                        <li><i class="fa fa-minus minimize-card"></i></li>
+                        <li><asp:LinkButton ID="Reload" runat="server" OnClick="Reload_Click"><i class="fa fa-refresh reload-card"></i></asp:LinkButton></li>
+                        <!--<li><i class="fa fa-trash close-card"></i></li>-->
+                    </ul>
+                </div>
+            </div>
+            <div class="card-block">
+                <div class="latest-update-box">
+                    <asp:Literal ID="Notifications" runat="server"></asp:Literal>
+                    <!--<div class="row p-b-30">
+                        <div class="col-auto text-right update-meta">
+                            <p class="text-muted m-b-0 d-inline">1 day ago</p>
+                            <i class="feather icon-check bg-simple-c-yellow  update-icon"></i>
+                        </div>
+                        <div class="col">
+                            <h6>Database backup completed!</h6>
+                            <p class="text-muted m-b-0">Download the <span class="text-c-blue">latest backup</span>.</p>
+                        </div>
+                    </div>-->
+                </div>
+                <div class="text-center">
+                    <a href="#!" class="b-b-primary text-primary">More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ Treatment/Pages/Setting/UserManagment/MyNotifications.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Treatment.Entity;
+using Website.Classes;
+
+namespace Treatment.Pages.Setting.UserManagment
+{
+    public partial class MyNotifications : System.Web.UI.Page
+    {
+        ECMSEntities db = new ECMSEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (SessionWrapper.LoggedUser == null)
+                Response.Redirect("~/Pages/Setting/Auth/Login.aspx");
+
+            int UserID = SessionWrapper.LoggedUser.Employee_Id;
+            List<Notification_Master> NotificationList = db.Notification_Master.Where(x => x.Employee_Structure_Id == UserID).ToList();
+            LoadNotification(NotificationList);
+        }
+
+        protected void Reload_Click(object sender, EventArgs e)
+        {
+            List<Treatment_Master> Treatment_List = db.Treatment_Master.ToList();
+            int UserID = SessionWrapper.LoggedUser.Employee_Id;
+            List<Notification_Master> NotificationList = db.Notification_Master.Where(x => x.Employee_Structure_Id == UserID).ToList();
+            LoadNotification(NotificationList);
+        }
+
+        public void LoadNotification(List<Notification_Master> List_Notify)
+        {
+            List<Treatment_Master> Treatment_List = db.Treatment_Master.ToList();
+            string str = string.Empty;
+            for (int i = 0; i < List_Notify.Count; i++)
+            {
+                int Structure_id = (int)List_Notify[i].Employee_Structure_Id;
+                Employee_Structure employeeStructure = db.Employee_Structure.FirstOrDefault(x => x.Employee_Structure_Id == Structure_id);
+                str += "<div class='row p-b-30'>" +
+                              "<div class='col-auto text-right update-meta'>" +
+                                "<p class='text-muted m-b-0 d-inline'>" + Date_Different((DateTime)List_Notify[i].Notification_Date) + "</p>" +
+                                "<i class='feather icon-twitter bg-info update-icon'></i>" +
+                            "</div>" +
+                            "<div class='col'>" +
+                                "<h6>" + employeeStructure.Employee.Employee_Name_En + "</h6>" +
+                                "<p class='text-muted m-b-0'>" + List_Notify[i].Notification_Description_En + "</p>" +
+                            "</div>" +
+                        "</div>";
+            }
+
+            Notifications.Text = str;
+        }
+
+        public string Date_Different(DateTime ReveviedDate)
+        {
+            string Different = "Unkown time ago";
+            try
+            {
+                // Get the current DateTime.
+                DateTime now = DateTime.Now;
+
+                // Get the TimeSpan of the difference.
+                TimeSpan elapsed = now.Subtract(ReveviedDate);
+
+                // Get number of days ago.
+                int Ago = (int)elapsed.TotalDays;
+                if (Ago < 1)
+                {
+                    Ago = (int)elapsed.TotalHours;
+
+                    if (Ago < 1)
+                    {
+                        Ago = (int)elapsed.TotalMinutes;
+                        if (Ago < 1)
+                        {
+                            Ago = (int)elapsed.Seconds;
+                            Different = Ago + " Seconds ago";
+                        }
+                        else
+                            Different = Ago + " Minutes ago";
+                    }
+                    else
+                        Different = Ago + " Hours ago";
+
+                }
+                else
+                    Different = Ago + " days ago";
+            }
+            catch { }
+
+            return Different;
+        }
+    }
+}
